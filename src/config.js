b756f9949af7097f265940d27bb5f1bbac76a44a
Christoffer Palsgaard <christofferpalsgaard@Christoffers-MacBook-Pro.local>
2021-07-19 15:07:29 +0200

    changed public ip to 52.51.148.88

--- src/config.js
+++ src/config.js
@@ -51,7 +51,7 @@ module.exports = {
       listenIps: [
         {
           ip: "0.0.0.0",
-          announcedIp: "127.0.0.1", // replace by public IP address
+          announcedIp: "52.51.148.88", // replace by public IP address
         },
       ],
       maxIncomingBitrate: 1500000,
